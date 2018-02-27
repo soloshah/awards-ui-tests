11f5ea8ef667589fb64051019a440c7822f84add
Neha Bharti <NEBHA@NHSBSA.NHS.UK>
2018-02-27 10:15:12 +0000

    Test Scenarios for BP490-134

---
+++ src/test/java/com/nhsbsa/finance/stepdefs/DepositAccountStepDefs.java
@@ -0,0 +1,156 @@
+package com.nhsbsa.finance.stepdefs;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.openqa.selenium.WebDriver;
+
+import com.nhsbsa.finance.driver.Config;
+import com.nhsbsa.finance.pageobjects.DepositAccountPage;
+import com.nhsbsa.finance.pageobjects.NavBarPage;
+import com.nhsbsa.finance.pageobjects.Page;
+import com.nhsbsa.finance.properties.PropertyReader;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class DepositAccountStepDefs {
+
+	private WebDriver driver = Config.getDriver();
+	private String baseUrl = PropertyReader.getProperty("base.server");
+	private DepositAccountPage depositAccountPage;
+	private String accountNumber;
+	private String rollNumber;
+	private String accountHolderName;
+	private String sortCodeFirstField;
+	private String sortCodeSecondField;
+	private String sortCodeThirdField;
+
+	@Given("^I am on the deposit account details page$")
+	public void iAmOnTheDepositAccountDetailsPage() {
+		new Page(driver).navigateToUrl(baseUrl + "/payment-details/deposit-account-details");
+	}
+
+	@Given("^I go to deposit account details page$")
+	public void iGoToTheDepositAccountDetailsPage() {
+		new Page(driver).navigateToUrl(baseUrl + "/payment-details/deposit-account-details");
+		depositAccountPage = new DepositAccountPage(driver);
+		 assertThat(depositAccountPage.getHeading()).contains("Enter bank details");
+	}
+
+	@Then("^the deposit account details submission will be successful$")
+	public void theDepositAccountDetailsSubmissionWillBeSuccessful() {
+		new NavBarPage(driver);
+	}
+
+	@When("^I enter valid deposit account details$")
+	public void IenterValidDepositAccountDetails() {
+		setBankDetails();
+		depositAccountPage = new DepositAccountPage(driver);
+		depositAccountPage.submitValidBankAccountDetails(accountNumber, rollNumber, sortCodeFirstField,
+				sortCodeSecondField, sortCodeThirdField, accountHolderName);
+	}
+
+	@Then("^the deposit account details submission will be unsuccessful$")
+	public void theDepositAccountDetailsSubmissionWillBeUnsuccessful() {
+		depositAccountPage = new DepositAccountPage(driver);
+		assertThat(depositAccountPage.getErrorHeadingErrorMessage())
+				.matches("Some questions have not been answered correctly.");
+		assertThat(depositAccountPage.getErrorsBelowErrorMessage()).matches("Please see the errors below.");
+	}
+
+	@And("^the account number error message '(.*)' will be displayed$")
+	public void theAccountNumberErrorMessageWillBeDisplayed(String errorMessage) {
+		depositAccountPage = new DepositAccountPage(driver);
+		assertThat(depositAccountPage.doesAccountNumberErrorMessageHaveAnchor()).isTrue();
+		assertThat(depositAccountPage.getAccountNumberAnchoredErrorMessage()).matches(errorMessage);
+		assertThat(depositAccountPage.getAccountNumberFieldErrorMessage()).matches(errorMessage);
+
+	}
+
+	@And("^the roll number error message '(.*)' will be displayed$")
+	public void theRollNumberErrorMessageWillBeDisplayed(String errorMessage) {
+		depositAccountPage = new DepositAccountPage(driver);
+		assertThat(depositAccountPage.doesRollNumberErrorMessageHaveAnchor()).isTrue();
+		assertThat(depositAccountPage.getRollNumberAnchoredErrorMessage()).matches(errorMessage);
+		assertThat(depositAccountPage.getRollNumberFieldErrorMessage()).matches(errorMessage);
+	}
+
+	@And("^the sort code error message '(.*)' will be displayed$")
+	public void theSortCoderErrorMessageWillBeDisplayed(String errorMessage) {
+		depositAccountPage = new DepositAccountPage(driver);
+		assertThat(depositAccountPage.doesSortCodeErrorMessageHaveAnchor()).isTrue();
+		assertThat(depositAccountPage.getSortCodeAnchoredErrorMessage()).matches(errorMessage);
+		assertThat(depositAccountPage.getSortCodeFieldErrorMessage()).matches(errorMessage);
+	}
+
+	@And("^the account holder name error message '(.*)' will be displayed$")
+	public void theAccountHolderNameErrorMessageWillBeDisplayed(String errorMessage) {
+		depositAccountPage = new DepositAccountPage(driver);
+		assertThat(depositAccountPage.doesAccountHolderNameErrorMessageHaveAnchor()).isTrue();
+		assertThat(depositAccountPage.getAccountHolderNameAnchoredErrorMessage()).matches(errorMessage);
+		assertThat(depositAccountPage.getAccountHolderNameFieldErrorMessage()).matches(errorMessage);
+	}
+
+	@When("^I enter invalid deposit account details using the (.*) '(.*)'$")
+	public void iEnterInvalidDepositAccountDetailsUsingFieldValue(String field, String value) {
+
+		setBankDetails();
+
+		switch (field) {
+		case "accountNumber":
+			accountNumber = value;
+			break;
+		case "rollNumber":
+			rollNumber = value;
+			break;
+		case "accountHolderName":
+			accountHolderName = value;
+			break;
+		}
+		depositAccountPage = new DepositAccountPage(driver);
+		depositAccountPage.enterBankDetails(accountNumber, rollNumber, sortCodeFirstField, sortCodeSecondField,
+				sortCodeThirdField, accountHolderName);
+		depositAccountPage.submitInValidBankDetails();
+	}
+
+	@Then("^The length of account holder name is verified$")
+	public void theLengthOfAccountHolderNameIsVerified() {
+		depositAccountPage = new DepositAccountPage(driver);
+		assertThat(depositAccountPage.readAccountHolderNameField()).matches("InvalidIn@ validInvalidI");
+
+	}
+	
+	@When("^I enter invalid sort code details using the sortCode1 '(.*)', sortCode2 '(.*)' and sortCode3 '(.*)'$")
+	public void iEnterInvalidSortCodeDetailsUsingTheSortCode1SortCode2AndSortCode3(String sortCode1, String sortCode2, String sortCode3) {
+		setBankAccountDetails();
+		depositAccountPage = new DepositAccountPage(driver);
+		depositAccountPage.enterBankDetails(accountNumber, rollNumber, sortCode1, sortCode2,
+				sortCode3, accountHolderName);
+		depositAccountPage.submitInValidBankDetails();
+	}
+
+	@When("^I enter invalid account holder name using the name '(.*)'$")
+	public void iEnterInvalidAccountHolderNameUsingTheName(String Name) {
+		depositAccountPage = new DepositAccountPage(driver);
+		depositAccountPage.enterAccountHolderName(Name);
+		depositAccountPage.submitInValidBankDetails();
+	}
+
+	private void setBankDetails() {
+		accountNumber = "12345678";
+		rollNumber = "AA-12345/9P";
+		accountHolderName = "Test-User";
+		sortCodeFirstField = "12";
+		sortCodeSecondField = "34";
+		sortCodeThirdField = "56";
+	}
+	
+	private void setBankAccountDetails(){
+		accountNumber = "12345678";
+		rollNumber = "AA-12345/9P";
+		accountHolderName = "Test-User";
+	}
+
+}
---
+++ src/test/resources/features/DepositAccount.feature
@@ -0,0 +1,74 @@
+@DepositAccount @BP490-134
+Feature: Deposit Account Details
+
+Background:
+Given I am on the start page
+When I go to deposit account details page
+
+
+Scenario: Valid Deposit Account Details
+And I enter valid deposit account details
+#Then the deposit account details submission will be successful
+#And bank account details page will be displayed
+Then the tbi page will be displayed
+
+Scenario Outline: Account number field validations 
+And I enter invalid deposit account details using the accountNumber '<accountNumber>'
+Then the deposit account details submission will be unsuccessful
+And the account number error message '<errorMessage>' will be displayed
+Examples:
+           |accountNumber | errorMessage                |
+           |              | Enter an account number     |   
+           | 123456       | Enter a valid account number|
+           |"£$%^&*@      | Enter a valid account number|
+           | ()12AYTO     | Enter a valid account number|
+           | 5678-AGU     | Enter a valid account number|
+           | 3456U78/.    | Enter a valid account number|
+
+Scenario Outline: Roll Number field validations 
+And I enter invalid deposit account details using the rollNumber '<rollNumber>'
+Then the deposit account details submission will be unsuccessful
+And the roll number error message '<errorMessage>' will be displayed
+Examples:
+           |rollNumber         | errorMessage             |
+           |                   | Enter a roll number      |   
+           | 123456U           | Enter a valid roll number|
+           |!"£$%^&*()@~}¬{+   | Enter a valid roll number|
+           | ()12A-/YTO        | Enter a valid roll number|
+           | 5678_AGU.U76      | Enter a valid roll number|
+           |AR/90 345678/.-893 | Enter a valid roll number|
+          
+       
+Scenario Outline: SortCode field validations 
+And I enter invalid sort code details using the sortCode1 '<sortCode1>', sortCode2 '<sortCode2>' and sortCode3 '<sortCode3>'
+Then the deposit account details submission will be unsuccessful
+And the sort code error message '<errorMessage>' will be displayed
+Examples:
+           | sortCode1 |sortCode2| sortCode3 | errorMessage           |
+           |           |         |           | Enter a sort code      | 
+           |           |  33     |  11       | Enter a valid sort code|
+           | 11        |         |  11       | Enter a valid sort code|
+           | 11        |  22     |           | Enter a valid sort code|
+           | ss        |ww       | rr        | Enter a valid sort code|
+           |  1        | 2       | 3         | Enter a valid sort code|
+           |  *"       |  £$     | &%        | Enter a valid sort code|
+           |  1Q       |  7U     |  0O       | Enter a valid sort code|
+                      
+                  
+Scenario Outline: Account Holder Name field validations 
+And I enter invalid deposit account details using the accountHolderName '<accountHolderName>'
+Then the deposit account details submission will be unsuccessful
+And the account holder name error message '<errorMessage>' will be displayed
+Examples:
+           |accountHolderName   | errorMessage                                             |
+           |                    | Enter the account holder's name                          |   
+           |!"£$%^&*()=¬@~}     | Enter names using only letters a to z, spaces and hyphens|
+           | 1234567            | Enter names using only letters a to z, spaces and hyphens|
+           | 12A/ YTO           | Enter names using only letters a to z, spaces and hyphens|
+           
+           
+Scenario: Account Holders name field length validations 
+When I enter invalid account holder name using the name 'InvalidIn@ validInvalidIn'
+Then The length of account holder name is verified
+And the deposit account details submission will be unsuccessful
+          
