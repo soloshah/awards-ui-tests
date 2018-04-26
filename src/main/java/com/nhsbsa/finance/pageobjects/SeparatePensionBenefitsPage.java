5880157e7ed580645884b0d9eef2fb5074ff5cff
Neha Bharti <NEBHA@NHSBSA.NHS.UK>
2018-04-26 11:29:15 +0100

    Test Scenarios for BP490-155

--- src/main/java/com/nhsbsa/finance/pageobjects/SeparatePensionBenefitsPage.java
+++ src/main/java/com/nhsbsa/finance/pageobjects/SeparatePensionBenefitsPage.java
@@ -5,15 +5,232 @@ import org.openqa.selenium.WebDriver;
 
 public class SeparatePensionBenefitsPage extends Page {
 
-  private String separatePensionBenefitsPageTitle = "Do you have any separate pension benefits? - Claim your NHS Pension - NHSBSA";
-  private By after6AprilCheckboxLocator = By.id("reason0-label-text");
+	private String separatePensionBenefitsPageTitle = "Do you have any separate pension benefits? - Claim your NHS Pension - NHSBSA";
+	private By after6AprilCheckboxLocator = By.id("reason0");
+	private By before6AprilCheckboxLocator = By.id("reason1");
+	private By noInformationCheckboxLocator = By.id("reason2");
+	private By noBenefitsCheckboxLocator = By.id("reason3");
+	private By annualLTAFieldLocator = By.id("percentInPaymentAfter2006");
+	private By benefitDayFieldLocator = By.id("firstBenefitCrysDate-day");
+	private By benefitMonthFieldLocator = By.id("firstBenefitCrysDate-month");
+	private By benefitYearFieldLocator = By.id("firstBenefitCrysDate-year");
+	private By grossAnnualRateFieldLocator = By.id("percentInPaymentBefore2006");
+	private By nextButtonLocator = By.id("submit_button");
+	private By backButtonLocator = By.id("back-link");
+	private By errorHeadingErrorMessageLocator = By.id("error-summary-heading");
+	private By errorsBelowErrorMessageLocator = By.id("error-summary-heading1");
+	private By after6AprilLTAFieldErrorMessageLocator = By.id("percentInPaymentAfter2006-error-message");
+	private By after6AprilLTAAnchoredErrorMessageLocator = By.id("error-list0");
+	private By after6AprilLTAAnchoredErrorMessageAnchorLocator = By.xpath("//a[@href='#percentInPaymentAfter2006']");
+	private By after6AprilDateFieldErrorMessageLocator = By.id("firstBenefitCrysDate-error-message");
+	private By after6AprilDateAnchoredErrorMessageLocator = By.id("error-list1");
+	private By after6AprilDateAnchoredErrorMessageAnchorLocator = By.xpath("//a[@href='#firstBenefitCrysDate']");
+	private By before6AprilFieldErrorMessageLocator = By.id("percentInPaymentBefore2006-error-message");
+	private By beforeAprilAnchoredErrorMessageLocator = By.id("error-list2");
+	private By before6AprilAnchoredErrorMessageAnchorLocator = By.xpath("//a[@href='#percentInPaymentBefore2006']");
+	private By separateBenefitsFieldErrorMessageLocator = By.id("reason-error-message");
+	private By separateBenefitsAnchoredErrorMessageLocator = By.id("error-list0");
+	private By separateBenefitsAnchoredErrorMessageAnchorLocator = By.xpath("//a[@href='#reason0']");
 
-  public SeparatePensionBenefitsPage(WebDriver driver) {
-    super(driver);
-    waitForTitleToExist(separatePensionBenefitsPageTitle);
-    waitForElementToBeVisibleBy(after6AprilCheckboxLocator);
-   
-  }
+	public SeparatePensionBenefitsPage(WebDriver driver) {
+		super(driver);
+		waitForTitleToExist(separatePensionBenefitsPageTitle);
+		waitForElementToBeVisibleBy(backButtonLocator);
 
-  
+	}
+
+	public void selectAfter6April() {
+		navigateToRootElement();
+		navigateToElementBy(after6AprilCheckboxLocator);
+		click();
+	}
+
+	public void selectBefore6April() {
+		navigateToRootElement();
+		navigateToElementBy(before6AprilCheckboxLocator);
+		click();
+	}
+
+	public void selectNoInformation() {
+		navigateToRootElement();
+		navigateToElementBy(noInformationCheckboxLocator);
+		click();
+	}
+
+	public void selectNoSeparateBenefits() {
+		navigateToRootElement();
+		navigateToElementBy(noBenefitsCheckboxLocator);
+		click();
+	}
+
+	public void enterCombinedLTA(String LTA) {
+		navigateToRootElement();
+		navigateToElementBy(annualLTAFieldLocator);
+		type(LTA);
+	}
+
+	public void enterGrossAnnualRate(String annualRate) {
+		navigateToRootElement();
+		navigateToElementBy(grossAnnualRateFieldLocator);
+		type(annualRate);
+	}
+
+	private void enterBenefitCrystallisationDay(String day) {
+		navigateToRootElement();
+		navigateToElementBy(benefitDayFieldLocator);
+		type(day);
+	}
+
+	private void enterBenefitCrystallisationMonth(String month) {
+		navigateToRootElement();
+		navigateToElementBy(benefitMonthFieldLocator);
+		type(month);
+	}
+
+	private void enterBenefitCrystallisationYear(String year) {
+		navigateToRootElement();
+		navigateToElementBy(benefitYearFieldLocator);
+		type(year);
+	}
+
+	public void enterBenefitCrystallisationDate(String day, String month, String year) {
+		enterBenefitCrystallisationDay(day);
+		enterBenefitCrystallisationMonth(month);
+		enterBenefitCrystallisationYear(year);
+	}
+
+	private void nextStep() {
+		navigateToRootElement();
+		navigateToElementBy(nextButtonLocator);
+		click();
+	}
+
+	public String getErrorHeadingErrorMessage() {
+		navigateToRootElement();
+		navigateToElementBy(errorHeadingErrorMessageLocator);
+		return getElementText();
+	}
+
+	public String getErrorsBelowErrorMessage() {
+		navigateToRootElement();
+		navigateToElementBy(errorsBelowErrorMessageLocator);
+		return getElementText();
+	}
+
+	public boolean doesSeparateBenefitsErrorMessageHaveAnchor() {
+		navigateToRootElement();
+		navigateToElementBy(separateBenefitsAnchoredErrorMessageLocator);
+		navigateToParentElement();
+		return getPresenceOfElement(separateBenefitsAnchoredErrorMessageAnchorLocator);
+	}
+
+	public String getSeparateBenefitsAnchoredErrorMessage() {
+		navigateToRootElement();
+		navigateToElementBy(separateBenefitsAnchoredErrorMessageLocator);
+		return getElementText();
+	}
+
+	public String getSeparateBenefitFieldErrorMessage() {
+		navigateToRootElement();
+		navigateToElementBy(separateBenefitsFieldErrorMessageLocator);
+		return getElementText();
+	}
+
+	public boolean doesAnnualLTAErrorMessageHaveAnchor() {
+		navigateToRootElement();
+		navigateToElementBy(after6AprilLTAAnchoredErrorMessageLocator);
+		navigateToParentElement();
+		return getPresenceOfElement(after6AprilLTAAnchoredErrorMessageAnchorLocator);
+	}
+
+	public String getAnnualLTAAnchoredErrorMessage() {
+		navigateToRootElement();
+		navigateToElementBy(after6AprilLTAAnchoredErrorMessageLocator);
+		return getElementText();
+	}
+
+	public String getAnnualLTAFieldErrorMessage() {
+		navigateToRootElement();
+		navigateToElementBy(after6AprilLTAFieldErrorMessageLocator);
+		return getElementText();
+	}
+
+	public boolean doesBenefitDateErrorMessageHaveAnchor() {
+		navigateToRootElement();
+		navigateToElementBy(after6AprilDateAnchoredErrorMessageLocator);
+		navigateToParentElement();
+		return getPresenceOfElement(after6AprilDateAnchoredErrorMessageAnchorLocator);
+	}
+
+	public String getBenefitDateAnchoredErrorMessage() {
+		navigateToRootElement();
+		navigateToElementBy(after6AprilDateAnchoredErrorMessageLocator);
+		return getElementText();
+	}
+
+	public String getBenefitDateFieldErrorMessage() {
+		navigateToRootElement();
+		navigateToElementBy(after6AprilDateFieldErrorMessageLocator);
+		return getElementText();
+	}
+
+	public boolean doesGrossAnnualRateErrorMessageHaveAnchor() {
+		navigateToRootElement();
+		navigateToElementBy(beforeAprilAnchoredErrorMessageLocator);
+		navigateToParentElement();
+		return getPresenceOfElement(before6AprilAnchoredErrorMessageAnchorLocator);
+	}
+
+	public String getGrossAnnualRateAnchoredErrorMessage() {
+		navigateToRootElement();
+		navigateToElementBy(beforeAprilAnchoredErrorMessageLocator);
+		return getElementText();
+	}
+
+	public String getGrossAnnualRateFieldErrorMessage() {
+		navigateToRootElement();
+		navigateToElementBy(before6AprilFieldErrorMessageLocator);
+		return getElementText();
+	}
+
+	public void submitSeperateBenefit() {
+		nextStep();
+
+	}
+	
+	public SeparatePensionBenefitsPage submitInvalidDetails() {
+		nextStep();
+		return new SeparatePensionBenefitsPage(driver);
+	}
+	
+
+	public SeparatePensionBenefitsPage isSeparateBenefitNotSelected() {
+		nextStep();
+		return new SeparatePensionBenefitsPage(driver);
+	}
+
+	public boolean isSeparateBenefitCheckboxSelected() {
+		boolean selected = true;
+
+		if (!isElementSelected(after6AprilCheckboxLocator) && !isElementSelected(before6AprilCheckboxLocator)
+				&& !isElementSelected(noInformationCheckboxLocator) && !isElementSelected(noBenefitsCheckboxLocator))
+			selected = false;
+		return selected;
+	}
+
+	public LifetimeAllowancePage selectAfterApril6Details(String LTA, String day, String month, String year) {
+		selectAfter6April();
+		enterCombinedLTA(LTA);
+		enterBenefitCrystallisationDate(day, month, year);
+		nextStep();
+		return new LifetimeAllowancePage(driver);
+	}
+
+	public LifetimeAllowancePage selectBeforeApril6Details(String annualRate) {
+		selectBefore6April();
+		enterGrossAnnualRate(annualRate);
+		nextStep();
+		return new LifetimeAllowancePage(driver);
+	}
+
 }
--- src/test/java/com/nhsbsa/finance/stepdefs/SeparatePensionBenefitsStepDefs.java
+++ src/test/java/com/nhsbsa/finance/stepdefs/SeparatePensionBenefitsStepDefs.java
@@ -2,22 +2,198 @@ package com.nhsbsa.finance.stepdefs;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
+
 import org.openqa.selenium.WebDriver;
 
 import com.nhsbsa.finance.driver.Config;
+import com.nhsbsa.finance.pageobjects.Page;
 import com.nhsbsa.finance.pageobjects.SeparatePensionBenefitsPage;
+import com.nhsbsa.finance.properties.PropertyReader;
+import com.nhsbsa.finance.shared.SharedData;
+import com.nhsbsa.finance.shared.SharedMethods;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class SeparatePensionBenefitsStepDefs {
 
-  private WebDriver driver = Config.getDriver();
+	private String baseUrl = PropertyReader.getProperty("base.server");
+	private WebDriver driver = Config.getDriver();
 
-   private SeparatePensionBenefitsPage  separatePensionBenefitsPage;
+	private SeparatePensionBenefitsPage separatePensionBenefitsPage;
 
 	@Then("^the separate pension benefits page will be displayed$")
-  public void theSeparatePensionBenefitsPageWillBeDisplayed() {
+	public void theSeparatePensionBenefitsPageWillBeDisplayed() {
 		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
 		assertThat(separatePensionBenefitsPage.getHeading()).contains("Do you have any separate pension benefits?");
-  }
+	}
+
+	@When("^I go to separate benefits page$")
+	public void iGoToSeparateBenefitsPage() {
+		Page page = new Page(driver);
+		page.navigateToUrl(baseUrl + "/pension-arrangement-details/do-you-have-separate-pension-benefits");
+		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
+		assertThat(separatePensionBenefitsPage.getHeading()).contains("Do you have any separate pension benefits?");
+	}
+
+	@When("^I select after April 6 2006 option$")
+	public void iSelectAfterApril62006Option() {
+		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
+		separatePensionBenefitsPage.selectAfter6April();
+
+	}
+
+	@When("^I select before April 6 2006 option$")
+	public void iSelectBeforeApril62006Option() {
+		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
+		separatePensionBenefitsPage.selectBefore6April();
+
+	}
+
+	@When("^I select dont have information$")
+	public void IselectDontHaveInformation() {
+		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
+		separatePensionBenefitsPage.selectNoInformation();
+		separatePensionBenefitsPage.submitSeperateBenefit();
+	}
+
+	@When("^I select No I dont have separate benefits$")
+	public void IselectNoIDontHaveSeparateBenefits() {
+		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
+		separatePensionBenefitsPage.selectNoSeparateBenefits();
+		separatePensionBenefitsPage.submitSeperateBenefit();
+	}
+
+	@When("^I select after and before april 6 2006$")
+	public void IselectAfterAndBeforeApril62006() throws InterruptedException {
+		iSelectAfterApril62006Option();
+		iEnterTheTotalCombinedPercentageOfLifetimeAllowance();
+		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
+		LocalDate plusYear = LocalDate.now().minusYears(11);
+		SharedData.day = SharedMethods.formatDay(plusYear);
+		SharedData.month = SharedMethods.formatMonth(plusYear);
+		SharedData.year = SharedMethods.formatYear(plusYear);
+		separatePensionBenefitsPage.enterBenefitCrystallisationDate(SharedData.day, SharedData.month, SharedData.year);
+		iSelectBeforeApril62006Option();
+		iEnterTheGrossAnnualRate();
+	}
+	
+	@Then("^the separate benefits submission will be unsuccessful$")
+	public void theSeparateBenefitsSubmissionWillBeUnsuccessful() {
+		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
+		assertThat(separatePensionBenefitsPage.getErrorHeadingErrorMessage()).matches("Your form contains errors");
+		assertThat(separatePensionBenefitsPage.getErrorsBelowErrorMessage()).matches("Check your answer:");
+	}
+
+	@When("^I dont select any option$")
+	public void iDontSelectAnyOption() {
+
+		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
+		separatePensionBenefitsPage.isSeparateBenefitNotSelected();
+
+	}
+
+	@Then("^the default value of separate benefits will be blank$")
+	public void theDefaultValueOfSeparateBenefitsWillBeBlank() {
+		assertThat(separatePensionBenefitsPage.isSeparateBenefitCheckboxSelected()).isFalse();
+	}
+
+	@And("^the error message '(.*)' for separate benefits page will be displayed$")
+	public void theErrorMessageForSeparateBenefitsPageWillBeDisplayed(String errorMessage) {
+		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
+		assertThat(separatePensionBenefitsPage.doesSeparateBenefitsErrorMessageHaveAnchor()).isTrue();
+		assertThat(separatePensionBenefitsPage.getSeparateBenefitsAnchoredErrorMessage()).matches(errorMessage);
+		assertThat(separatePensionBenefitsPage.getSeparateBenefitFieldErrorMessage()).matches(errorMessage);
+	}
+
+	@And("^the error message '(.*)' for benefit crystallisation date will be displayed$")
+	public void theErrorMessageForBenefitCrystallisationDateWillBeDisplayed(String errorMessage) {
+		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
+		assertThat(separatePensionBenefitsPage.doesBenefitDateErrorMessageHaveAnchor()).isTrue();
+		assertThat(separatePensionBenefitsPage.getBenefitDateAnchoredErrorMessage()).matches(errorMessage);
+		assertThat(separatePensionBenefitsPage.getBenefitDateFieldErrorMessage()).matches(errorMessage);
+	}
+
+	@And("^the error message '(.*)' for gross annual rate will be displayed$")
+	public void theErrorMessageForGrossAnnualRateWillBeDisplayed(String errorMessage) {
+		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
+		assertThat(separatePensionBenefitsPage.doesGrossAnnualRateErrorMessageHaveAnchor()).isTrue();
+		assertThat(separatePensionBenefitsPage.getGrossAnnualRateAnchoredErrorMessage()).matches(errorMessage);
+		assertThat(separatePensionBenefitsPage.getGrossAnnualRateFieldErrorMessage()).matches(errorMessage);
+	}
+
+	@And("^the error message '(.*)' for annual LTA will be displayed$")
+	public void theErrorMessageForAnnualLTAWillBeDisplayed(String errorMessage) {
+		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
+		assertThat(separatePensionBenefitsPage.doesAnnualLTAErrorMessageHaveAnchor()).isTrue();
+		assertThat(separatePensionBenefitsPage.getAnnualLTAAnchoredErrorMessage()).matches(errorMessage);
+		assertThat(separatePensionBenefitsPage.getAnnualLTAFieldErrorMessage()).matches(errorMessage);
+	}
+
+	@When("^I enter the invalid date for benefit crystallisation using the day '(.*)', month '(.*)' and year '(.*)'$")
+	public void iEnterInvalidDateForBenefitCrystallisationUsingTheDayMonthAndYear(String day, String month,
+			String year) {
+
+		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
+		separatePensionBenefitsPage.enterBenefitCrystallisationDate(day, month, year);
+		separatePensionBenefitsPage.submitInvalidDetails();
+
+	}
+
+	@And("^I enter the benefit crystallisation date greater than today's date$")
+	public void iEnterTheBenefitCrystallisationDateGreaterThanTodaysDate() {
+		LocalDate futuredate = LocalDate.now().plusDays(1);
+		SharedData.day = SharedMethods.formatDay(futuredate);
+		SharedData.month = SharedMethods.formatMonth(futuredate);
+		SharedData.year = SharedMethods.formatYear(futuredate);
+		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
+		separatePensionBenefitsPage.enterBenefitCrystallisationDate(SharedData.day, SharedData.month, SharedData.year);
+		separatePensionBenefitsPage.submitInvalidDetails();
+	}
+
+	@When("^I enter the invalid gross annual rate using '(.*)'$")
+	public void iEnterTheInvalidGrossAnnualRatesUsing(String annualRate) throws InterruptedException {
+		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
+		separatePensionBenefitsPage.enterGrossAnnualRate(annualRate);
+		separatePensionBenefitsPage.submitInvalidDetails();
+		Thread.sleep(3000);
+	}
+
+	@When("^I enter the invalid total percentage of LTA using '(.*)'$")
+	public void iEnterTheInvalidTotalPercentageOfLTAUsing(String LTA) {
+		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
+		separatePensionBenefitsPage.enterCombinedLTA(LTA);
+
+	}
+
+	@When("^I enter the total combined percentage of lifetime allowance$")
+	public void iEnterTheTotalCombinedPercentageOfLifetimeAllowance() {
+		String LTA = "123456.78";
+		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
+		separatePensionBenefitsPage.enterCombinedLTA(LTA);
+	
+	}
+
+	@When("^I enter the gross annual rate$")
+	public void iEnterTheGrossAnnualRate() {
+		String annualRate = "987654.98";
+		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
+		separatePensionBenefitsPage.enterGrossAnnualRate(annualRate);
+		separatePensionBenefitsPage.submitSeperateBenefit();
+	
+	}
+
+	@And("^I enter the date of first benefit crystallisation$")
+	public void iEnterTheDateOfFirstBenefitCrystallisation() {
+		LocalDate plusYear = LocalDate.now().minusYears(11);
+		SharedData.day = SharedMethods.formatDay(plusYear);
+		SharedData.month = SharedMethods.formatMonth(plusYear);
+		SharedData.year = SharedMethods.formatYear(plusYear);
+		separatePensionBenefitsPage = new SeparatePensionBenefitsPage(driver);
+		separatePensionBenefitsPage.enterBenefitCrystallisationDate(SharedData.day, SharedData.month, SharedData.year);
+		separatePensionBenefitsPage.submitSeperateBenefit();
+	}
+
 }
